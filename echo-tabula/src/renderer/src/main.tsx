79ce9f57b1a40bcb153c27b0c5d8be8091a4ff39
HHShiro <64503929+RafaLBarros@users.noreply.github.com>
2026-03-29 07:12:04 -0300

    Removido Sandbox

--- echo-tabula/src/renderer/src/main.tsx
+++ echo-tabula/src/renderer/src/main.tsx
@@ -3,11 +3,9 @@ import './assets/main.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { Sandbox } from './Sandbox';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App /> {/* Comentamos o App para focar na bancada de testes */}
-    {/*<Sandbox />*/} {/* Renderize a bancada em vez do App */}
+    <App />
   </StrictMode>
 )
